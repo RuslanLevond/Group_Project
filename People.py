d5fe5b2c7cdb424e8a76dd78a4e2c4e5a91480ce
kirilengovski <engovski.kiril@outlook.com>
2018-10-22 13:14:26 +0100

    New

---
+++ People.py
@@ -0,0 +1,66 @@
+people_child = {
+
+    "id": "child",
+
+    "name": "a lonely child",
+
+    "description": "Alone child sitting on the bench and looking in the sky",
+
+    "knowledge": "Sorry, I haven't seen anything."
+}
+
+ people_homeless = {
+
+    "id": "homeless",
+
+    "name": "a homeless man",
+
+    "description": "An exhausted homeless man, probably on drugs, laying on the floor",
+
+    "knowledge": "Yes, I saw someone running like a dragon is chasing him. He went south!"
+}
+
+people_junkie = {
+	
+    "id": "junkie",
+
+    "name": "a high junkie",
+
+    "description": "A junkie laying on the floor, seems unconscious",
+
+    "knowledge": "Leave me alone."
+}
+
+people_old_lady = {
+	
+    "id": "lady",
+
+    "name": "an old lady",
+
+    "description": "A very old, short, lady walking very slowly with her walking stick",
+
+    "knowledge": "Oh son, I think they went south!"
+}
+
+people_mafia = {
+	
+    "id": "mafia",
+
+    "name": "a mafia looking man",
+
+    "description": "A man dressed in full black suit, wearing black gloves and holding a suitcase",
+
+    "knowledge": " - Just stares at you without saying anything - "
+}
+
+people_priest = {
+	
+    "id": "priest",
+
+    "name": "a priest",
+
+    "description": "A man wearing a clerical shirt and a detachable clerical collar",
+
+    "knowledge": "Yes son, they went south!"
+}
+
