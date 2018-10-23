ef26f1218d668cce8fbf1735cd2625d5d3c6b8f5
kirilengovski <engovski.kiril@outlook.com>
2018-10-23 11:28:12 +0100

    sry

---
+++ Enemies.py
@@ -0,0 +1,38 @@
+enemy_guard_outside = {
+    "name" : "guard",
+    "health" : 3,
+    "base_health" : 3,
+    "damage" : 1
+    }
+enemy_receptionist = {
+    "name" : "receptionist",
+    "health" : 2,
+    "base_health" : 2,
+    "damage" : 1
+    }
+enemy_guard_toilet = {
+    "name" : "guard",
+    "health" : 4,
+    "base_health" : 4,
+    "damage" : 1
+    }
+enemy_guard_floor1 = {
+    "name" : "guard",
+    "health" : 3,
+    "base_health" : 3,
+    "damage" : 2
+    }
+enemy_guard_security = {
+    "name" : "guard",
+    "health" : 5,
+    "base_health" : 5,
+    "damage" : 1
+    }
+enemy_BIGBOSS = {
+    "name" : "big boss",
+    "health" : 8,
+    "base_health" : 8,
+    "damage" : 2
+    }
+
+## to call the function will be attacks(enemies[guard_outside])
